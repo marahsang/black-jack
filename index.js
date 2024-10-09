4444622d8bbe2e4054ae05bdcef5d1a0b084913f
Maraah <maraahsang@gmail.com>
2024-10-09 14:43:19 +0000

    Define variables and conditionals for black jack

---
+++ index.js
@@ -0,0 +1,15 @@
+let firstCard = 7
+let secondCard = 11
+let sum = firstCard + secondCard
+
+if(sum <= 20) {
+    console.log("Do you want to draw a new card? 🌝")
+}   else if (sum === 21) {
+    console.log("Woohoo you've got blackjack 😇")
+
+}   else {
+    console.log("You are out of the game 😦")
+}
+
+
+
