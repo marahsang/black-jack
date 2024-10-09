83584a5379ce5796d7563b8cd7284a87961573f6
Maraah <maraahsang@gmail.com>
2024-10-09 15:16:54 +0000

    Create variable that tracks whether user has black jack

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-let firstCard = 7
+let firstCard = 10
 let secondCard = 11
 let sum = firstCard + secondCard
-
+let hasBlackJack = false
 if(sum <= 20) {
     console.log("Do you want to draw a new card? 🌝")
 }   else if (sum === 21) {
     console.log("Woohoo you've got blackjack 😇")
-
+    hasBlackJack = true
+    console.log(hasBlackJack)
 }   else {
     console.log("You are out of the game 😦")
 }
