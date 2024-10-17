0f7d3349d7cce2b128ced8ee043e0a440fe3b0ed
Maraah <maraahsang@gmail.com>
2024-10-17 13:55:34 +0000

    Update basic styling to Blackjack game

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-let firstCard = 10
+let firstCard = 7
+let secondCard = 11
+let sum = firstCard + secondCard
+hasBlackJack = false
+isAlive = true
+let message = ""
+if(sum <= 20) {
+    message = "Do you want to draw a new card? 🌝"
+}   else if (sum === 21) {
+    message = "Woohoo you've got blackjack 😇"
+    hasBlackJack = true
+
+}   else {
+    message = "You are out of the game 😦"
+    isAlive = false
+}
+
+console.log(message)
+
+
+
+
+/* let firstCard = 10
 let secondCard = 12
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
+let message = ""
+message = "Woohoo you've got blackjack 😇"
 if(sum <= 20) {
     console.log("Do you want to draw a new card? 🌝")
 }   else if (sum === 21) {
@@ -10,10 +34,10 @@ if(sum <= 20) {
     hasBlackJack = true
     console.log(hasBlackJack)
 }   else {
-    console.log("You are out of the game 😦")
+    console.log("You are out of the game 😦") 
     isAlive = false
     console.log(isAlive)
 }
+console.log(message)
 
-
-
+*/
