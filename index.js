d82b17d39acc6f3801c313ef0422b58b1a6a8120
Maraah <maraahsang@gmail.com>
2024-10-28 10:02:24 +0000

    Display the message and the sum on the page
    Store the message-el paragraph in a variable called messageEl and display the message in the messageEl using messageEl.textContent. The procedure is repeated for the sum variable.

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-let firstCard = 7
+let firstCard = 10
 let secondCard = 11
 let sum = firstCard + secondCard
 hasBlackJack = false
 isAlive = true
 let message = ""
 let starEL = document.getElementById("start-el")
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+
 function startGame () {
+    sumEl.textContent = "Sum: " + sum
     if(sum <= 20) {
         message = "Do you want to draw a new card? 🌝"
     }   else if (sum === 21) {
@@ -16,7 +20,7 @@ function startGame () {
         message = "You are out of the game 😦"
         isAlive = false
     }
-    console.log(message)
+    messageEl.textContent = message
     }
 
 
